9344ad10ef0f087c59a0623a24bd30c408248d91
l2vrns <lorans0alkhalailah@gmail.com>
2024-12-26 17:02:30 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,61 @@
+// question 1
+
+console.log(-5 * 3);  //  -15
+
+console.log("JavaScript" + 50);  //  javaScript50 
+
+console.log(17 % 5);    //  2
+
+console.log(5 % 17);    //  5
+
+console.log(5 / 10);     //  0.5
+
+console.log(4 === "4");    //   false
+
+console.log(4 != 5);       // true
+
+console.log(7 <= 8);      // true
+
+console.log("Hello" + 5);     //Hello5
+
+let x = 10;
+
+console.log(Math.ceil(x) - Math.floor(x));     // 0
+
+console.log(Math.pow(x, 2));     //     100
+
+// question 2
+
+let y = prompt("enter a number!");
+
+alert("your nubmer is " + y);
+
+// question 3
+
+let num1 = prompt("enter a your first number");
+
+let num2 = prompt("enter a your second number");
+
+alert("your summation is " + (Number(num1) + Number(num2)));
+
+// question 4
+
+let multi1 = prompt("enter a your first number");
+
+let multi2 = prompt("enter a your second number");
+
+alert("your summation is " + Number(multi1) * Number(multi2));
+
+// question 5
+
+let div1 = prompt("enter a your first number");
+
+let div2 = prompt("enter a your second number");
+
+alert("your summation is " + Number(div1) / Number(div2));
+
+let first = 10;
+
+let second = 25;
+
+alert(first + second);
